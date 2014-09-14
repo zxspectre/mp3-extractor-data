f0cd3b84b26e9a7bc0b1bb35ea3fc1b4198ad51f
Nayil <disproper@gmail.com>
2014-09-15 01:28:22 +0400

    neuralnet in R

--- sample/loadmatdata.R
+++ sample/loadmatdata.R
@@ -1,12 +1,38 @@
 library("stringr")
 library("R.matlab")
+library("neuralnet")
+
+createneuralnet <- function(){
+  #use registerDoMC 
+  nn.data<-loadmatdata()  
+  nn.data.vars<-paste(names(nn.data)[1:ncol(nn.data)-1],"",collapse="+")
+  nn.expr=paste(names(nn.data)[ncol(nn.data)],"~",nn.data.vars)
+  print(summary(nn.data$label))
+  #net<-neuralnet(nn.expr,nn.data,hidden=0,rep=1,err.fct="ce", linear.output=FALSE)
+  net<-neuralnet(nn.expr,nn.data,hidden=0,rep=1,err.fct="sse", linear.output=FALSE)
+}
 
 loadmatdata <- function(){
-  matfiles<-list.files(path = ".", pattern = ".*\\.mat", all.files = FALSE,
+  ##TODO: pass list of vectors (1st = fileprefix, 2nd = label number)
+  ## 0 is for HEAVY
+  matfiles<-list.files(path = ".", pattern = "^heavy.*\\.mat", all.files = FALSE,
            full.names = FALSE, recursive = FALSE,
            ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
-  alldata<-sapply(matfiles,processmatfile)
-  alldata<-do.call(rbind, alldata)
+  alldata1<-lapply(matfiles,processmatfile)
+  alldata1<-do.call(rbind, alldata1)
+  label<-rep(0,dim(alldata1)[1])
+  nn.data1<-data.frame(alldata1,label)
+
+  #1 is for POWER
+  matfiles<-list.files(path = ".", pattern = "^power.*\\.mat", all.files = FALSE,
+                       full.names = FALSE, recursive = FALSE,
+                       ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+  alldata2<-lapply(matfiles,processmatfile)
+  alldata2<-do.call(rbind, alldata2)
+  label<-rep(1,dim(alldata2)[1])
+  nn.data2<-data.frame(alldata2,label)
+  
+  alldata<-rbind(nn.data1,nn.data2)
   return(alldata)
 }
 
@@ -14,8 +40,11 @@ processmatfile <- function(filename){
   varname<-str_match(filename,"(.*?)\\.mat")[2]
   data<-readMat(filename)
   varsize<-dim(data[[varname]])
+  print(filename)
   output<-matrix(data[[varname]],ncol=varsize[2],nrow=varsize[1])
   res<-t(output)
-  names(res)<-varname
+  #names(res)<-varname
   return(res)
 }
+
+
